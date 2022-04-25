4a15200cee94b18498c80a55ab65408679cc56a1
user <user@DESKTOP-DB9U1HD>
2022-04-25 15:05:06 +0900

    첫번째 테스트

---
+++ test/src/main/java/com/user/UserDTO.java
@@ -0,0 +1,16 @@
+package com.user;
+
+public class UserDTO {
+	private String userId;
+	
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+
+}
